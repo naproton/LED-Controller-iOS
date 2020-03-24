29c81b3759919b9a8388b2611082aa04ad995de0
Nick Proton <nprotonentis@eg-gilero.com>
2020-03-24 14:10:33 -0400

    Initial Commit

---
+++ LED Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LED Controller
+//
+//  Created by Nick Protonentis on 3/24/20.
+//  Copyright © 2020 Nick Protonentis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LED ControllerTests/LED_ControllerTests.swift
@@ -0,0 +1,34 @@
+//
+//  LED_ControllerTests.swift
+//  LED ControllerTests
+//
+//  Created by Nick Protonentis on 3/24/20.
+//  Copyright © 2020 Nick Protonentis. All rights reserved.
+//
+
+import XCTest
+@testable import LED_Controller
+
+class LED_ControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
